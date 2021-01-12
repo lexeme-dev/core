0d94e86579f3f26bef05c549ba56de6dbdf156f1
Faiz Surani <faiz.surani@gmail.com>
2021-01-11 23:02:10 -0800

    Make progress on role extraction of citation graph

--- case_clustering.py
+++
@@ -1,7 +0,0 @@
-from db_models import db, Opinion
-from construct_graph import construct_graph
-from networkx.algorithms import community
-
-citation_graph = construct_graph()
-partitions = community.k_clique_communities(citation_graph, 50)
-print("")
---
+++ case_structural_roles.py
@@ -0,0 +1,26 @@
+from construct_graph import construct_graph
+import networkx as nx
+import pickle
+from helpers import get_names_for_id_collection
+
+from graphrole import RecursiveFeatureExtractor, RoleExtractor
+
+citation_graph = construct_graph()
+feature_extractor = RecursiveFeatureExtractor(citation_graph)
+features = feature_extractor.extract_features()
+role_extractor = RoleExtractor(n_roles=None)
+role_extractor.extract_role_factors(features)
+node_roles = role_extractor.roles
+
+print('\nNode role assignments:')
+print(node_roles)
+
+print('\nNode role membership by percentage:')
+print(role_extractor.role_percentage.round(2))
+# TODO: When I come back to this, use the debugger console to save the generated data structures to disk (using pickle, I believe)
+print("Done.")
+
+# with open("data/role_data.pik", "wb") as f:
+#     pickle.dump({"citation_graph": citation_graph, "feature_extractor": feature_extractor, "features": features,
+#                  "role_extractor": role_extractor, "node_roles": node_roles, "role_percentages":
+#                      role_extractor.role_percentage.round(2)}, f, -1)
