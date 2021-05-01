4713b79fb7b3f0757ca6b927e6a8b9ef51e51d22
Faiz Surani <faiz.surani@gmail.com>
2021-05-01 08:55:34 -0700

    Add top_n helper

--- helpers.py
+++ helpers.py
@@ -1,3 +1,6 @@
+import heapq
+from collections import OrderedDict
+from typing import Dict
 from db_models import db, Opinion, Cluster
 
 
@@ -10,3 +13,15 @@ def get_names_for_id_collection(collection):
         except:
             names.append("Unknown")
     return names
+
+
+def top_n(value_dict: dict, n: int) -> Dict[str, float]:
+    """Helper function to find the n highest-value keys in a dictionary.
+    Runs in O(n+k) time for a dictionary with k entries."""
+    # Have to reformat the dict like this for heapq to cooperate.
+    collection = [(value, key) for key, value in value_dict.items()]
+    heapq.heapify(collection)
+    top_n_items = OrderedDict()
+    for nth_largest in heapq.nlargest(n, collection):
+        top_n_items[nth_largest[1]] = nth_largest[0]  # Reconstruct the dict
+    return top_n_items
