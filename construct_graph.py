e91c4b6c680b366c2dbc151e6995e35383aea4cc
Faiz Surani <faiz.surani@gmail.com>
2021-05-01 08:55:25 -0700

    Add construct_graph flag to make directed or undirected

--- construct_graph.py
+++ construct_graph.py
@@ -5,8 +5,11 @@ import pickle
 MAX_DEPTH = 122  # To normalize lowest edge weight to 1
 
 
-def construct_graph() -> nx.Graph:
-    citation_graph = nx.Graph()
+def construct_graph(directed=False):
+    if directed:
+        citation_graph = nx.DiGraph()
+    else:
+        citation_graph = nx.Graph()
     db.connect()
     citations = [(c.citing_opinion, c.cited_opinion, 1 / c.depth) for c in Citation.select()]
     db.close()
