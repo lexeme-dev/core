f7869e1709853cea937e5a5d6ea43393f97424ac
Faiz Surani <faiz.surani@gmail.com>
2021-05-02 18:14:48 -0700

    Implement unified single/group case similarity endpoint

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
+from http import HTTPStatus
 from db.db_models import Opinion, Cluster, Similarity
 from graph.citation_network import CitationNetwork
 from playhouse.shortcuts import model_to_dict
@@ -13,18 +14,16 @@ def get_case(resource_id: int):
         opinion = Opinion.get(resource_id=resource_id)
         return model_to_dict(opinion)
     except Opinion.DoesNotExist:
-        abort(404)
+        abort(HTTPStatus.NOT_FOUND)
 
 
-@app.route('/cases/<int:resource_id>/similar')
-def similar_cases(resource_id: int):
-    get_case(resource_id)
-    query = Similarity.select() \
-        .join(Opinion, on=Similarity.opinion_b).join(Cluster) \
-        .where(Similarity.opinion_a == resource_id) \
-        .order_by(Similarity.similarity_index.desc()) \
-        .limit(25)
-    similar_cases = [similarity_record.opinion_b for similarity_record in query]
+@app.route('/cases/similar')
+def similar_cases_to_group():
+    case_resource_ids = request.args.getlist('cases')
+    if len(case_resource_ids) < 1:
+        return "You must provide at least one case ID.", HTTPStatus.UNPROCESSABLE_ENTITY
+    similar_case_query = citation_graph.similarity.db_case_similarity(tuple(case_resource_ids))
+    similar_cases = [similarity_record.opinion_b for similarity_record in similar_case_query]
     return jsonify(list(map(model_to_dict, similar_cases)))
 
 
