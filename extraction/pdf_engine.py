cf8481a0b705653cbb48703c1dac7b3689e70286
Faiz Surani <faiz.surani@gmail.com>
2021-05-10 16:56:20 -0700

    Implement basic PDF text extraction class

---
+++ extraction/pdf_engine.py
@@ -0,0 +1,12 @@
+from typing import BinaryIO
+import pdfminer.high_level
+
+
+class PdfEngine:
+    file: BinaryIO
+
+    def __init__(self, file):
+        self.file = file
+
+    def get_text(self) -> str:
+        return pdfminer.high_level.extract_text(self.file)
