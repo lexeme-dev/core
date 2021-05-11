4573732d3f045fcb1befec6b6124c54e4cff93ce
Varun Iyer <varun_iyer@posteo.net>
2021-05-11 13:27:43 -0700

    Added casebrief_from_resource_id
    This is better because it uses the standard resource id we have instead
    of full cites.

--- casebrief.py
+++ casebrief.py
@@ -1,7 +1,9 @@
 import requests
 import re
+from itertools import chain
 
-CLURL = "https://www.courtlistener.com/api/rest/v3/search/?q={}&type=o"
+CLSEARCH = "https://www.courtlistener.com/api/rest/v3/search/?q={}&type=o"
+CLID = "https://www.courtlistener.com/api/rest/v3/opinions/{}"
 
 
 def _hardstrip(s):
@@ -11,15 +13,28 @@ def _hardstrip(s):
     """
     return re.sub(r"[^\w]", "", s).lower()
 
+def cl_get_from_resource_id(rid):
+    req = requests.get(CLID.format(rid))
+    if not req:
+        raise ValueError(f"Could not find resource id {rid} in CourtListener!")
+    case_init = req.json()
+    cluster = requests.get(case_init["cluster"]).json()
+    docket = requests.get(cluster["docket"]).json()
+    case = {}
+    # this has to be done bc of the way python's default cat operators work
+    for k, v in chain(case_init.items(), cluster.items(), docket.items()):
+        if not (k in case and case[k]):
+            case[k] = v
+    return case
 
-def cl_get(citation):
+def cl_get_from_cite(citation):
     """
     Searches a citation and returns its data from courtlistener.
     Must be a full bluebook cite (with reporter)
     """
     if not re.search("\d", citation):
         raise ValueError("Must be a full bluebook cite with reporter, volume, and page!")
-    req = requests.get(CLURL.format(citation))
+    req = requests.get(CLSEARCH.format(citation))
     if not req:
         raise ValueError("Could not find citation in CourtListener!")
     values = req.json()
@@ -51,7 +66,18 @@ def oyez_brief(*args):
         return None
 
 
-def casebrief(citation):
-    cl = cl_get(citation)
+def casebrief_from_cite(citation):
+    cl = cl_get_from_cite(citation)
     year = int(re.findall("\d{4}", citation)[-1])
-    return oyez_brief(year, cl["docketNumber"])
+    try:
+        return oyez_brief(year, cl["docketNumber"])
+    except KeyError:
+        return None
+
+def casebrief_from_resource_id(rid):
+    cl = cl_get_from_resource_id(rid)
+    try:
+        year = int(re.findall("\d{4}", cl["date_filed"])[-1])
+        return oyez_brief(year, cl["docket_number"])
+    except KeyError:
+        return None
