13a828a15b97955a92e9466e8acddf09b03cb32c
Faiz Surani <faiz.surani@gmail.com>
2021-06-05 02:58:07 -0400

    Add multiple-input case recommendation algorithm

--- graph/case_recommendation.py
+++ graph/case_recommendation.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from math import sqrt, log
 from graph.network_edge_list import NetworkEdgeList
 from graph.random_walker import RandomWalker
 from helpers import top_n
@@ -17,6 +18,22 @@ class CaseRecommendation:
     def __init__(self, network_edge_list: NetworkEdgeList):
         self.network_edge_list = network_edge_list
         self.random_walker = RandomWalker(self.network_edge_list)
+
+    def recommendations(self, opinion_ids: set, num_recommendations,
+                        max_walk_length=MAX_WALK_LENGTH, max_num_steps=MAX_NUM_STEPS) -> Dict[str, float]:
+        query_case_weights = self.input_case_weights(opinion_ids)
+        print(query_case_weights)
+        overall_node_freq_dict = {}
+        for opinion_id, weight in query_case_weights.items():
+            curr_max_num_steps = int(weight * max_num_steps)
+            curr_freq_dict = self.recommendations_for_case(int(opinion_id), num_recommendations=None,
+                                                           max_walk_length=max_walk_length,
+                                                           max_num_steps=curr_max_num_steps)
+            for node, freq in curr_freq_dict.items():
+                if node not in overall_node_freq_dict:
+                    overall_node_freq_dict[node] = 0
+                overall_node_freq_dict[node] += sqrt(freq)  # See Eq. 3 of Eksombatchai et. al (2018)
+        return top_n(overall_node_freq_dict, num_recommendations)
 
     def recommendations_for_case(self, opinion_id, num_recommendations,
                                  max_walk_length=MAX_WALK_LENGTH, max_num_steps=MAX_NUM_STEPS) -> Dict[str, float]:
@@ -46,3 +63,30 @@ class CaseRecommendation:
         if opinion_id in node_freq_dict:
             del node_freq_dict[opinion_id]
         return top_n(node_freq_dict, num_recommendations)
+
+    def input_case_weights(self, opinion_ids) -> Dict[str, float]:
+        """
+        Given a set of opinion IDs in a query, give the probability distribution with which to visit them based on
+        their degree centralities.
+
+        :param opinion_ids: A set of opinion IDs
+        :return:
+        """
+        total_num_edges, max_degree = 0, 0
+        node_degrees = {}
+        for op_id in opinion_ids:
+            node_metadata = self.network_edge_list.node_metadata[op_id]
+            node_degree = node_metadata['end'] - node_metadata['start']
+            node_degrees[op_id] = node_degree
+            total_num_edges += node_degree
+            if node_degree > max_degree:
+                max_degree = node_degree
+        if total_num_edges == 0:
+            return {op_id: 0 for op_id in opinion_ids}
+        denormalized_weights = {op_id: self.denormalized_case_weight(node_degree, max_degree, total_num_edges)
+                                for op_id, node_degree in node_degrees.items()}
+        denormalized_weight_sum = sum(denormalized_weights.values())
+        return {op_id: node_weight / denormalized_weight_sum for op_id, node_weight in denormalized_weights.items()}
+
+    def denormalized_case_weight(self, node_degree, max_degree, total_num_edges):
+        return (node_degree * (max_degree - log(node_degree))) / total_num_edges
