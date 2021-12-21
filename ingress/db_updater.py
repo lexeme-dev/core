33e138966b8c37b5d6d339287809e0f899cdec9d
Faiz Surani <faiz.surani@gmail.com>
2021-12-20 18:33:25 -0800

    Make various bug fixes to ingress DbUpdater
    It works!!!

--- ingress/db_updater.py
+++ ingress/db_updater.py
@@ -16,6 +16,7 @@ from helpers import get_full_path
 from ingress.paths import BASE_CL_DIR, CLUSTER_PATH, OPINION_PATH, CITATIONS_PATH
 
 DEFAULT_BATCH_SIZE = 10000
+HTML_TEXT_FIELDS = ['html_with_citations', 'html', 'html_lawbox', 'html_columbia', 'html_anon_2020', 'plain_text']
 
 
 class DbUpdater:
@@ -80,11 +81,13 @@ class DbUpdater:
                         cluster_records.append(new_record)
             except:
                 print(f'Failure on file {file}')
-        print(f"Finished reading CL cluster data for jurisdiction {jurisdiction}, upserting {len(cluster_records)} records...")
+        print(
+            f"Finished reading CL cluster data for jurisdiction {jurisdiction}, upserting {len(cluster_records)} records...")
         self.__batch_query(self.__upsert_clusters_to_db, cluster_records)
 
     def process_opinion_data(self, dir_path: str, include_text: bool, jurisdiction: str):
         opinion_checksum_dict = self.__get_opinion_checksum_dict()
+        cluster_checksum_dict = self.__get_cluster_checksum_dict()
         opinion_records = []
         directory = os.fsencode(dir_path)
         for file in os.listdir(directory):
@@ -103,36 +106,53 @@ class DbUpdater:
                         opinion_data = json.loads(file_contents.decode('utf-8'))
                         cluster_uri = opinion_data['cluster']
                         cluster_id = int(cluster_uri.split('/')[-2])
+                        # To avoid foreign key violations
+                        if cluster_id not in cluster_checksum_dict:
+                            continue
+
                         new_record = dict(resource_id=opinion_data['id'],
                                           opinion_uri=opinion_data['resource_uri'],
                                           cluster_uri=cluster_uri,
-                                          cluster=cluster_id,
+                                          cluster_id=cluster_id,
                                           courtlistener_json_checksum=file_checksum)
                         if include_text:
-                            new_record.html_text = opinion_data['html_text']
+                            new_record['html_text'] = self.__get_html_text(opinion_data)
                         opinion_records.append(new_record)
             except:
                 print(f'Failure on file {file}')
-        print(f"Finished reading CL opinion data for jurisdiction {jurisdiction}, upserting {len(opinion_records)} records...")
+        print(
+            f"Finished reading CL opinion data for jurisdiction {jurisdiction}, upserting {len(opinion_records)} records...")
         batch_size = 100 if include_text else DEFAULT_BATCH_SIZE
         self.__batch_query(self.__upsert_opinions_to_db, opinion_records, batch_size=batch_size)
 
     def process_citation_data(self, citations_file):
-        opinion_set = set(self.session.execute(select(Opinion.resource_id)).scalars().all())
+        opinion_checksum_dict = self.__get_opinion_checksum_dict()
+        # Big memory drinker, but worth it for the speed increase unless it becomes a problem.
+        citation_set = set(self.session.execute(select(Citation.citing_opinion_id, Citation.cited_opinion_id, Citation.depth)).all())
 
         citation_records = []
         with open(citations_file) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
+            next(csv_reader)
             for row in csv_reader:
                 try:
                     citing_opinion, cited_opinion, depth = int(row[0]), int(row[1]), int(row[2])
-                    if citing_opinion in opinion_set and cited_opinion in opinion_set:
-                        new_record = Citation(citing_opinion=citing_opinion, cited_opinion=cited_opinion,
-                                              depth=depth)
+                    if (citing_opinion, cited_opinion, depth) in citation_set:
+                        continue
+                    if citing_opinion in opinion_checksum_dict and cited_opinion in opinion_checksum_dict:
+                        new_record = dict(citing_opinion_id=citing_opinion, cited_opinion_id=cited_opinion,
+                                          depth=depth)
                         citation_records.append(new_record)
                 except Exception as e:
                     print(f'Failure on citation file row {row}: {e}')
-            self.__batch_query(self.__upsert_citations_to_db, citation_records)
+        print(f"Finished reading CL citation data, upserting {len(citation_records)} records...")
+        self.__batch_query(self.__upsert_citations_to_db, citation_records)
+
+    def __get_html_text(self, opinion_data):
+        for field in HTML_TEXT_FIELDS:
+            if opinion_data.get(field) and len(opinion_data[field]) > 0:
+                return opinion_data[field]
+        return None
 
     def __get_reporter(self, cluster_data):
         reporters = cluster_data.get('citations')
@@ -168,7 +188,7 @@ class DbUpdater:
         self.session.execute(query)
 
     def __upsert_opinions_to_db(self, clusters: List[dict]):
-        query = insert(Cluster).values(clusters)
+        query = insert(Opinion).values(clusters)
         query = query.on_conflict_do_update(index_elements=[Opinion.resource_id],
                                             set_=dict(cluster_id=query.excluded.cluster_id,
                                                       cluster_uri=query.excluded.cluster_uri,
