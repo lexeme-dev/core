2d665950de7b901f82fa898c15389af88c5fb1f4
Faiz Surani <faiz.surani@gmail.com>
2022-02-09 23:57:41 +0100

    Switch from re to regex lib

--- extraction/parenthetical_processor.py
+++ extraction/parenthetical_processor.py
@@ -1,17 +1,15 @@
-import re
+import regex
 
 
 # noinspection RegExpUnnecessaryNonCapturingGroup,SpellCheckingInspection
 class ParentheticalProcessor:
-    __MODIFIABLE = r"(?:omissions?|quotations?|quotes?|(?:quotations? )?marks?|ellips.s|cites?|citations?|emphas.s|italics?|footnotes?|alterations?|punctuation|modifications?|brackets?|bracketed material|formatting)"
-    __MODIFABLE_TYPE = (
-        r"(?:internal|former|latter|first|second|last|some|further|certain|numbered)"
-    )
+    __MODIFIABLE = r"(?:omissions?|quotations?|quotes?|headings?|(?:quotations? )?marks?|ellips.s|cites?|citations?|emphas.s|italics?|footnotes?|alterations?|punctuation|modifications?|brackets?|bracketed material|formatting)"
+    __MODIFABLE_TYPE = r"(?:internal|former|latter|first|second|last|some|further|certain|numbered|other|transcript)"
     __FULL_MODIFIABLE = f"(?:(?:{__MODIFABLE_TYPE} )?{__MODIFIABLE})"
-    __QUOTE_MODIFICATION = r"(?:added|removed|adopted|(?:in )?(?:the )original|omitted|included|deleted|eliminated|supplied|ours|changed|in \S+|by \S+ court)"
-    __DOCUMENT_TYPES = r"(?:opinion|continuance|order|op\.|decision|case|disposition|authority|authoritie)"
-    __OPINION_TYPES = r"(?:separate|supplemental|amended|majority|dissent|dissenting|concurrence|concurring|plurality|unpublished|revised|per curi.m|in.chambers|judgment|joint|principal)"
-    __OPINION_TYPE_MODIFICATION = r"(?:in (?:the )?(?:judgment|result)s?(?: in part)?|in result|in part|in relevant part|(?:from|with) .{1,25})"
+    __QUOTE_MODIFICATION = r"(?:added|provided|removed|adopted|(?:in )?(?:the )original|omitted|included|deleted|eliminated|altered|modified|supplied|ours|mine|changed|(?:in|by) \S+|by \S+ court)"
+    __DOCUMENT_TYPES = r"(?:opinion|continuance|order|op\.|decision|case|disposition|authority|authoritie|statement)"
+    __OPINION_TYPES = r"(?:separate|supplemental|amended|majority|dissent|dissenting|concurrence|concurring|plurality|unpublished|revised|per curi.m|in.chambers|judgment|joint|principal|panel|unanimous|5-4)"
+    __OPINION_TYPE_MODIFICATION = r"(?:in (?:the )?(?:judgment|result)s?(?: in part)?|in result|in part|dubitante|in relevant part|(?:from|with) .{1,25})"  # TIL: https://en.wikipedia.org/wiki/Dubitante
     __FULL_OPINION_DESCRIPTOR = (
         f"(?:{__OPINION_TYPES}(?: {__OPINION_TYPE_MODIFICATION})?)"
     )
@@ -27,11 +25,14 @@ class ParentheticalProcessor:
         f"{__JUDGE_NAME}(?: {__FULL_OPINION_DESCRIPTOR})?(?: ?,?(?:and|,) {__FULL_OPINION_DESCRIPTOR})*",  # Scalia, J., dissenting; Roberts, C.J., concurring in the judgment
         f"(?:{__DOCUMENT_TYPES} )?{__FULL_OPINION_DESCRIPTOR}",  # concurring in result
         f"{__DOCUMENT_TYPES} of {__JUDGE_NAME}",  # opinion of Breyer, J.; opinion of Scalia and Alito, J.J.
-        f"{__OPINION_TYPES}(?: {__DOCUMENT_TYPES})?",  # plurality opinion
+        f"{__OPINION_TYPES}(?: {__DOCUMENT_TYPES})?(?: {__OPINION_TYPE_MODIFICATION})?",  # plurality opinion
         r"dictum|dicta",
+        r"on rehearing|denying cert(iorari)?",
         r"simplified|cleaned up|as amended",
         r"same|similar|contra",
-        r"and cases cited therein",
+        r"(?:and )?cases cited therein",
+        r"No. \d+.?\d+",  # No. 12-456
+        r"n. \d+",  # n. 6
         f"{__DOCUMENT_TYPES} below",  # case below
         f"{__AGGREGATOR_TYPES} {__DOCUMENT_TYPES}s?",  # collecting cases, reviewing cases
         f"(?:{__OPINION_TYPES} )?table(?: {__DOCUMENT_TYPES})?",
@@ -43,31 +44,31 @@ class ParentheticalProcessor:
         r".{1,10}\d{4}",  # 2nd Cir. 2019
         r".{1,25} I+",  # Gonzales II
         r".{1,10} (?:Circuit|Cir.)",  # Tenth Circuit, 5th Cir.
-        r'here(?:in)?after .+',  # hereinafter, "Griffin II"
+        r"here(?:in)?after .+",  # hereinafter, "Griffin II"
         r"\S*",  # Single-word parentheticals, e.g., 'TILA'
     ]
 
     __PREFIX = rf"^{__SURROUNDING_CHARS}*(?:"  # Begin string, optional whitespace, optional puncutation, begin capture group
     __SUFFIX = rf"){__SURROUNDING_CHARS}*$"  # Close capture group, optional punctuation, optional whitespace, end string
 
-    PARENTHETICAL_BLACKLIST_REGEX = re.compile(
+    PARENTHETICAL_BLACKLIST_REGEX = regex.compile(
         __PREFIX
         + r"|".join(map(lambda reg: f"(?:{reg})", PARENTHETICAL_REGEX_BLACKLIST_RULES))
         + __SUFFIX,
-        re.IGNORECASE,
+        regex.IGNORECASE,
     )
 
     @classmethod
     def is_descriptive(cls, text: str) -> bool:
         text = cls.prepare_text(text)
-        return not re.match(cls.PARENTHETICAL_BLACKLIST_REGEX, text)
+        return not regex.match(cls.PARENTHETICAL_BLACKLIST_REGEX, text)
 
     @classmethod
     def prepare_text(cls, text: str) -> str:
-        text = re.sub(r"\* ?\d+", " ", text)  # Remove star page numbers (e.g. *389)
-        text = re.sub(r" _{4,} ", " ", text)  # Remove weird "word ----- word" stuff
-        text = re.sub(r"\s+", " ", text)  # Remove excess whitespace
-        text = re.sub(r"[“”]", '"', text)  # Use ASCII quotes
+        text = regex.sub(r"\* ?\d+", " ", text)  # Remove star page numbers (e.g. *389)
+        text = regex.sub(r" _{4,} ", " ", text)  # Remove weird "word ----- word" stuff
+        text = regex.sub(r"\s+", " ", text)  # Remove excess whitespace
+        text = regex.sub(r"[“”]", '"', text)  # Use ASCII quotes
         return text
 
 
